8bf2d033e28eee8526d1bf891526a846217718ee
mehedi <mehedi4595@gmail.com>
2023-10-19 11:45:02 +0600

    Sign Up usename and email verify or check with authentication

--- authentication/views.py
+++ authentication/views.py
@@ -4,11 +4,13 @@ from .models import *
 #login authentication
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate,login,logout
-
 # Create your views here.
 def signupPage(request):
     error_messages = {
+        'username_error': 'Username already exitest.',
         'password_error': 'Password and Confirm Password not match',
+        'login_error': 'Invalid username or password. Please try again.',
+        'email_error': 'Email Already exited',
     }
     if request.method == "POST":
         uname = request.POST.get("name")
@@ -16,7 +18,20 @@ def signupPage(request):
         pass1 = request.POST.get("password")
         pass2 = request.POST.get("confirmpassword")
 
-        if pass1!= pass2:
+        # User verification
+        existing_user = customUser.objects.filter(username=uname).first()
+        existing_email = customUser.objects.filter(email=email).first()
+
+        if existing_user:
+            messages.error(request, error_messages['username_error'])
+        elif existing_email:
+            messages.error(request, error_messages['email_error'])
+        # else:
+        #     user =authenticate(request, username=uname, email=email)
+
+        #User veryfy end
+
+        elif pass1!= pass2:
              messages.error(request, error_messages['password_error'])
         else:
             # Use your customUser model to create a user
@@ -24,7 +39,6 @@ def signupPage(request):
             myuser.save()
             return redirect("loginPage")
     return render(request, "signup.html")
-
 
 
 
@@ -38,14 +52,12 @@ def loginPage(request):
     if request.method == "POST":
         username = request.POST.get("username")
         pass1 = request.POST.get("password")
-        
         if not username:
             messages.error(request, error_messages['username_error'])
         elif not pass1:
             messages.error(request, error_messages['password_error'])
         else:
-            user =authenticate(request, username=username, password=pass1,)
-
+            user =authenticate(request, username=username, password=pass1)
             if user is not None:
                 login(request,user)
                 user_type = user.user_type
@@ -59,29 +71,19 @@ def loginPage(request):
                     return redirect("signupPage")
             else:
                 messages.error(request, error_messages['login_error'])
-
     return render(request, "login.html")
 
 def logoutPage(request):
     logout(request)
     return redirect("loginPage")
-
 def adminPage(request):
-
     return render(request,"myAdmin/adminhome.html")
-
-
 def myProfile(request):
     user = request.user
     context = {
         'user': user
     }
     return render(request, 'profile.html', context)
-
-
-
-
-
 #Profile Update 
 def profileUpdate(request):
     error_messages = {
@@ -89,7 +91,6 @@ def profileUpdate(request):
         'error': 'Profile Not Updated',
         'password_error': 'Current password is incorrect',
     }
-    
     if request.method == "POST":
         profile_pic = request.FILES.get('profile_pic')
         firstname = request.POST.get("first_name")
@@ -97,14 +98,11 @@ def profileUpdate(request):
         password = request.POST.get("password")
         username = request.POST.get("username")
         email = request.POST.get("email")
-        
         try:
             cuser = customUser.objects.get(id=request.user.id)
-            
             cuser.first_name = firstname
             cuser.last_name = lastname
             cuser.profile_pic = profile_pic
-            
             # Verify the current password provided matches the user's current password
             if not cuser.check_password(password):
                 messages.error(request, error_messages['password_error'])
@@ -121,10 +119,7 @@ def profileUpdate(request):
                 return redirect("profileUpdate")
         except:
             messages.error(request, error_messages['error'])
-    
     return render(request, 'profile.html')
-
-
 #Change Password
 def changePassword(request):
     error_messages = {
@@ -132,13 +127,11 @@ def changePassword(request):
         'mismatch': 'New password and confirm password not matched',
         'old_password': 'Old password not match',
     }
-    
     if request.method == "POST":
         old_password = request.POST.get("oldPassword")
         new_password = request.POST.get("newpassword")
         confirm_password = request.POST.get("confirmPassword")
         user = request.user
-        
         if user.check_password(old_password):
             if new_password == confirm_password:
                 user.set_password(new_password)
@@ -149,5 +142,32 @@ def changePassword(request):
                 messages.error(request, error_messages['mismatch'])
         else:
             messages.error(request, error_messages['old_password'])
-
     return render(request, "changepassword.html")
+
+
+
+
+
+
+
+# def signupPage(request):
+#     error_messages = {
+#         'password_error': 'Password and Confirm Password not match',
+#     }
+#     if request.method == "POST":
+#         uname = request.POST.get("name")
+#         email = request.POST.get("email")
+#         pass1 = request.POST.get("password")
+#         pass2 = request.POST.get("confirmpassword")
+        
+
+#         if pass1!= pass2:
+#              messages.error(request, error_messages['password_error'])
+#         else:
+#             # Use your customUser model to create a user
+#             myuser = customUser.objects.create_user(username=uname, email=email, password=pass1)
+#             myuser.save()
+#             return redirect("loginPage")
+#     return render(request, "signup.html")
+
+
